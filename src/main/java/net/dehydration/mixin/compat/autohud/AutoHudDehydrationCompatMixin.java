671f173b98072b6a834252b2485465e35138febe
August Saint Freytag <me@augustfreytag.com>
2025-05-31 18:46:31 +0200

    Remove deprecated injection logic for AutoHUD

--- src/main/java/net/dehydration/mixin/compat/autohud/AutoHudDehydrationCompatMixin.java
+++ src/main/java/net/dehydration/mixin/compat/autohud/AutoHudDehydrationCompatMixin.java
@@ -1,27 +1,15 @@
 package net.dehydration.mixin.compat.autohud;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import mod.crend.autohud.compat.DehydrationCompat;
 import net.dehydration.mixinlogic.compat.autohud.AutoHudDehydrationCompatMixinLogic;
-import net.minecraft.client.network.ClientPlayerEntity;
 
 @Mixin(DehydrationCompat.class)
 public abstract class AutoHudDehydrationCompatMixin implements AutoHudDehydrationCompatMixinLogic {
-
-	@Overwrite
-	public void init() {
-		mixinInit();
-	}
-
-	@Overwrite
-	public void tickState(ClientPlayerEntity player) {
-		mixinTickState(player);
-	}
 
 	@Inject(method = "<clinit>", at = @At("HEAD"), cancellable = true)
 	private static void overwriteStaticBlock(CallbackInfo ci) {
--- src/main/java/net/dehydration/mixinlogic/compat/autohud/AutoHudDehydrationCompatMixinLogic.java
+++ src/main/java/net/dehydration/mixinlogic/compat/autohud/AutoHudDehydrationCompatMixinLogic.java
@@ -8,41 +8,9 @@ import mod.crend.autohud.component.Component;
 import mod.crend.autohud.component.Components;
 import mod.crend.autohud.component.state.PolicyComponentState;
 import mod.crend.autohud.render.ComponentRenderer;
-import net.dehydration.Mod;
 import net.dehydration.access.HydrationManagerAccess;
-import net.dehydration.misc.ThirstTooltipData;
-import net.dehydration.mod.ModEffects;
-import net.minecraft.client.network.ClientPlayerEntity;
 
 public interface AutoHudDehydrationCompatMixinLogic {
-
-	default void mixinInit() {
-		if (Component.getComponents().contains(DehydrationCompat.Thirst)) {
-			Mod.LOGGER.info("Thirst component already registered with AutoHUD, skipping.");
-			return;
-		}
-
-		Mod.LOGGER.info("Registering thirst component with AutoHUD.");
-
-		var thirstComponent = DehydrationCompat.Thirst;
-		Components.Hunger.addStackComponent(thirstComponent);
-	}
-
-	default void mixinTickState(ClientPlayerEntity player) {
-		var thirstComponent = DehydrationCompat.Thirst;
-
-		if (player.hasStatusEffect(ModEffects.THIRST)) {
-			thirstComponent.reveal();
-		} else if (((HydrationManagerAccess) player).getHydrationManager().isNotFull()) {
-			if (!player.getMainHandStack().isEmpty() && player.getMainHandStack().getTooltipData().isPresent()
-					&& player.getMainHandStack().getTooltipData().get() instanceof ThirstTooltipData) {
-				thirstComponent.reveal();
-			} else if (!player.getOffHandStack().isEmpty() && player.getOffHandStack().getTooltipData().isPresent()
-					&& player.getOffHandStack().getTooltipData().get() instanceof ThirstTooltipData) {
-				thirstComponent.reveal();
-			}
-		}
-	}
 
 	static void mixinStatic() {
 		// Overwrite the static block's behavior
